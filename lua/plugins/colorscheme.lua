8ddfee82c907be47de85965ae36772687782cf0c
ticktessly <eepyashell@gmail.com>
2026-09-11 21:09:47 +0800

    tokyonight moon is so good idk why

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,9 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "folke/tokyonight.nvim",
+  lazy = false,
   priority = 1000,
+  opts = {},
   config = function()
-    vim.cmd.colorscheme("catppuccin")
+    vim.cmd.colorscheme("tokyonight-moon")
   end,
 }
