da0786fcd785214779ead2ccb90b85beae880e07
ticktessly <eepyashell@gmail.com>
2026-09-16 19:55:03 +0800

    i dont see anything different from using the preconfigured gopls from lspconfig

--- lua/lang/go.lua
+++ lua/lang/go.lua
@@ -12,22 +12,6 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		opts = function()
-			vim.lsp.config("gopls", {
-				settings = {
-					gopls = {
-						hints = {
-							assignVariableTypes = true,
-							compositeLiteralFields = true,
-							compositeLiteralTypes = true,
-							constantValues = true,
-							functionTypeParameters = true,
-							parameterNames = true,
-							rangeVariableTypes = true,
-						},
-					},
-				},
-			})
-
 			vim.lsp.enable("gopls")
 		end,
 	},
