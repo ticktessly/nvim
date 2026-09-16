852b417311b8dc993ea5d1472510f8a013e9263b
ticktessly <eepyashell@gmail.com>
2026-09-16 19:55:17 +0800

    is this some kind of formatting thingy? what the hell changed

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,4 @@
 local map = vim.keymap.set
-map("i", "jj", "<Esc>", { silent = true, noremap = true })
+map("i", "jj", "<Esc>", { desc = "Exit insert mode", silent = true, noremap = true })
 map("t", "``", "<C-\\><C-n>", { desc = "Exit terminal mode" })
-map("n", "<leader>l", "<cmd>Lazy<cr>", { silent = true, noremap = true })
+map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Open Lazy", silent = true, noremap = true })
