1324c0cbc9cba242509a46ce237b9d058a6141d4
ticktessly <eepyashell@gmail.com>
2026-09-17 16:05:20 +0800

    why is scroll enabled by default? btw im still trying out explorer

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -102,36 +102,36 @@ return {
 			require("telescope").load_extension("fzf")
 		end,
 	},
-	{
-		"nvim-mini/mini.files",
-		version = false,
-		keys = {
-			{
-				"<leader>e",
-				function()
-					require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
-				end,
-				desc = "Open mini.files (current file)",
-			},
-			{
-				"<leader>E",
-				function()
-					require("mini.files").open(vim.uv.cwd(), true)
-				end,
-				desc = "Open mini.files (cwd)",
-			},
-		},
-		opts = {
-			windows = {
-				preview = true,
-				width_focus = 30,
-				width_preview = 30,
-			},
-			options = {
-				use_as_default_explorer = true,
-			},
-		},
-	},
+	-- {
+	-- 	"nvim-mini/mini.files",
+	-- 	version = false,
+	-- 	keys = {
+	-- 		{
+	-- 			"<leader>e",
+	-- 			function()
+	-- 				require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+	-- 			end,
+	-- 			desc = "Open mini.files (current file)",
+	-- 		},
+	-- 		{
+	-- 			"<leader>E",
+	-- 			function()
+	-- 				require("mini.files").open(vim.uv.cwd(), true)
+	-- 			end,
+	-- 			desc = "Open mini.files (cwd)",
+	-- 		},
+	-- 	},
+	-- 	opts = {
+	-- 		windows = {
+	-- 			preview = true,
+	-- 			width_focus = 30,
+	-- 			width_preview = 30,
+	-- 		},
+	-- 		options = {
+	-- 			use_as_default_explorer = true,
+	-- 		},
+	-- 	},
+	-- },
 	{
 		"akinsho/bufferline.nvim",
 		version = "*",
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,10 +3,19 @@ return {
 	lazy = false,
 	opts = {
 		dashboard = { enabled = true },
+		scroll = { enabled = false },
+		explorer = { enabled = true },
 		indent = { enabled = true },
 		lazygit = { enabled = true },
 	},
 	keys = {
+		{
+			"<leader>e",
+			function()
+				Snacks.explorer()
+			end,
+			desc = "Explorer",
+		},
 		{
 			"<leader>gg",
 			function()
