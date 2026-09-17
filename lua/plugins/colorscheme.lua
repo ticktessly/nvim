9a7ace365e3e6546832a6ab6ca871d7e89299c55
ticktessly <eepyashell@gmail.com>
2026-09-17 21:17:14 +0800

    deadass this is what i want from koda, like supports for many plugins

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,18 @@
 -- return {
--- 	"oskarnurm/koda.nvim",
+-- 	"folke/tokyonight.nvim",
 -- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 -- 	priority = 1000, -- make sure to load this before all the other start plugins
 -- 	config = function()
--- 		vim.cmd("colorscheme koda")
+-- 		vim.cmd("colorscheme tokyonight")
 -- 	end,
 -- }
+--
 return {
-	"folke/tokyonight.nvim",
-	lazy = false, -- make sure we load this during startup if it is your main colorscheme
-	priority = 1000, -- make sure to load this before all the other start plugins
+	"bjarneo/vantablack.nvim",
+	lazy = false,
+	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme tokyonight")
+		require("vantablack").setup()
+		vim.cmd.colorscheme("vantablack")
 	end,
 }
