82779eeb283d5f95b2f1244571c9d38a207f3c0a
ticktessly <eepyashell@gmail.com>
2026-09-17 15:50:33 +0800

    im still choosing between koda and tokyonight

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,8 +1,16 @@
+-- return {
+-- 	"oskarnurm/koda.nvim",
+-- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+-- 	priority = 1000, -- make sure to load this before all the other start plugins
+-- 	config = function()
+-- 		vim.cmd("colorscheme koda")
+-- 	end,
+-- }
 return {
-	"oskarnurm/koda.nvim",
+	"folke/tokyonight.nvim",
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		vim.cmd("colorscheme koda")
+		vim.cmd("colorscheme tokyonight")
 	end,
 }
