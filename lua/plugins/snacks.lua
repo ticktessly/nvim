28c4f491f3ddade5a96b0e78fe831be1c1d85dc9
ticktessly <eepyashell@gmail.com>
2026-09-17 16:15:50 +0800

    replaces telescope with snacks picker

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,19 @@ return {
 	"folke/snacks.nvim",
 	lazy = false,
 	opts = {
+		picker = {
+			sources = {
+				explorer = {
+					hidden = true,
+					ignored = true,
+					layout = {
+						layout = {
+							position = "right",
+						},
+					},
+				},
+			},
+		},
 		dashboard = { enabled = true },
 		scroll = { enabled = false },
 		explorer = { enabled = true },
@@ -22,6 +35,55 @@ return {
 				Snacks.lazygit()
 			end,
 			desc = "LazyGit",
+		},
+		{
+			"<leader>fb",
+			function()
+				Snacks.picker.buffers()
+			end,
+			desc = "Buffers",
+		},
+		{
+			"<leader>fc",
+			function()
+				Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+			end,
+			desc = "Find Config File",
+		},
+		{
+			"<leader>ff",
+			function()
+				Snacks.picker.files()
+			end,
+			desc = "Find Files",
+		},
+		{
+			"<leader>fg",
+			function()
+				Snacks.picker.grep()
+			end,
+			desc = "Find Git Files",
+		},
+		{
+			"<leader>fp",
+			function()
+				Snacks.picker.projects()
+			end,
+			desc = "Projects",
+		},
+		{
+			"<leader>fr",
+			function()
+				Snacks.picker.recent()
+			end,
+			desc = "Recent",
+		},
+		{
+			"<leader><space>",
+			function()
+				Snacks.picker.smart()
+			end,
+			desc = "Smart Find Files",
 		},
 	},
 }
