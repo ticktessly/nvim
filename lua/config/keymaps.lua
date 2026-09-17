590e7936a6c08155d02b1b21a0da53e80a42cd68
ticktessly <eepyashell@gmail.com>
2026-09-17 16:04:49 +0800

    since i use snacks picker, it's now like, splitting the screen into two which then needs these maps for moving from the explorer to the file

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,7 @@ local map = vim.keymap.set
 map("i", "jj", "<Esc>", { desc = "Exit insert mode", silent = true, noremap = true })
 map("t", "``", "<C-\\><C-n>", { desc = "Exit terminal mode" })
 map("n", "<leader>l", "<cmd>Lazy<cr>", { desc = "Open Lazy", silent = true, noremap = true })
+map("n", "<C-h>", "<C-w>h", { desc = "Go to Left Window", remap = true })
+map("n", "<C-j>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
+map("n", "<C-k>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
+map("n", "<C-l>", "<C-w>l", { desc = "Go to Right Window", remap = true })
