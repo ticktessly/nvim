2458f46353484758df64279594b38bd279ae810a
ticktessly <eepyashell@gmail.com>
2026-09-19 08:34:56 +0800

    tokyonight ig

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,18 +1,18 @@
--- return {
--- 	"folke/tokyonight.nvim",
--- 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
--- 	priority = 1000, -- make sure to load this before all the other start plugins
--- 	config = function()
--- 		vim.cmd("colorscheme tokyonight")
--- 	end,
--- }
---
 return {
-	"bjarneo/vantablack.nvim",
-	lazy = false,
-	priority = 1000,
+	"folke/tokyonight.nvim",
+	lazy = false, -- make sure we load this during startup if it is your main colorscheme
+	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		require("vantablack").setup()
-		vim.cmd.colorscheme("vantablack")
+		vim.cmd("colorscheme tokyonight")
 	end,
 }
+--
+-- return {
+-- 	"bjarneo/vantablack.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("vantablack").setup()
+-- 		vim.cmd.colorscheme("vantablack")
+-- 	end,
+-- }
