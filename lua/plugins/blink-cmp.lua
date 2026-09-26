5be54a50919f624448112071c777d148ac2daac9
ticktessly <eepyashell@gmail.com>
2026-09-27 01:08:16 +0800

    lazydev for vim api autocomp. blink cmp also got rounded corners
    i know there's like a way to do this with vim lsp config librry tinkery
    stuff but womp womp.

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -1,16 +1,44 @@
 return {
 	"saghen/blink.cmp",
-	dependencies = { "rafamadriz/friendly-snippets" },
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+	},
 	version = "1.*",
 	event = { "InsertEnter", "CmdlineEnter" },
 	opts = {
 		keymap = { preset = "super-tab" },
 		sources = {
-			default = { "lsp", "path", "snippets", "buffer" },
+			-- Add lazydev to the default list of sources
+			default = { "lazydev", "lsp", "path", "snippets", "buffer" },
+			providers = {
+				lazydev = {
+					name = "LazyDev",
+					module = "lazydev.integrations.blink",
+					-- make lazydev completions top priority (score_offset >= 0)
+					score_offset = 100,
+				},
+			},
+		},
+		completion = {
+			menu = {
+				border = "rounded",
+			},
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 200,
+				window = {
+					border = "rounded",
+				},
+			},
 		},
 		cmdline = {
 			keymap = { preset = "super-tab" },
-			completion = { menu = { auto_show = true } },
+			completion = {
+				menu = {
+					auto_show = true,
+					border = "rounded",
+				},
+			},
 		},
 	},
 }
